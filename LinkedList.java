b25372c94b8f750763ffd48c3c7f875249ff990b
vishalgaur752 <vishalgaur752@gmail.com>
2024-05-31 19:10:30 +0530

    InstaDSA

---
+++ LinkedList.java
@@ -0,0 +1,79 @@
+public class LinkedList {
+    // public ListNode midddleNode(ListNode head) {
+    // ListNode slow = head;
+    // ListNode fast = head;
+    // while(fast != null && fast.next != null) {
+    // slow = slow.next;
+    // fast = fast.next.next;
+    // }
+    // return slow;
+    // }
+    public class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("ll is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public Node middleNode(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        // ll.print();
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+        Node middle = ll.middleNode(head);
+        System.out.println(middle.data);
+
+    }
+}
